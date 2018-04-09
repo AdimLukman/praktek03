8c1428b0a53624858f69b1b8e0214cf504a2dc43
AdimLukman <32480217+AdimLukman@users.noreply.github.com>
2018-04-09 23:16:46 +0800

    Getter And Setter

--- src/praktek03/SepedaGunung.java
+++ src/praktek03/SepedaGunung.java
@@ -1,5 +1,18 @@
 package praktek03;
 
-public class SepedaGunung {
-    
-}
+public class SepedaGunung extends SepedaMotor{
+        private String jenisSG;
+        
+        void throtle (){
+        System.out.println("ccuusssss....");
+    }
+
+    public String getJenisSG() {
+        return jenisSG;
+    }
+
+    public void setJenisSG(String jenisSG) {
+        this.jenisSG = jenisSG;
+    }
+
+}  
