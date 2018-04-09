636b1809c55a15df7f25245eeadb94358b89f86a
AdimLukman <32480217+AdimLukman@users.noreply.github.com>
2018-04-09 22:46:13 +0800

    Atribut dan Method

--- src/praktek03/SepedaMotor.java
+++ src/praktek03/SepedaMotor.java
@@ -1,5 +1,10 @@
 package praktek03;
 public class SepedaMotor extends sepeda{
+    private String jenisMotor;
+    
+    void throttle (){
+        System.out.println("NGEEEENG....");
+    }
     
     
 }
